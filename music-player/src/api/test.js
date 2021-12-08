dce42a79b9bdab02e46121ecd2cee203a5478f10
HOLLYwyh <1689394668@qq.com>
2021-12-08 22:56:36 +0800

    add

--- music-player/src/api/test.js
+++ music-player/src/api/test.js
@@ -3,7 +3,7 @@ import request from '@/utils/request'
 export function testAPI (params) {
   return request({
     url: '/api/test/songTest',
-    method: 'get',
+    method: 'GET',
     params
   })
 }
